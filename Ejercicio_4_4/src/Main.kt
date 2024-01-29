c924529b78b878ec39b51b4d6471dc78a5c5e977
lkru144 <lkru144@g.educaand.es>
2024-01-29 17:53:59 +0100

    EJERCICIOS POO I mains terminados

--- Ejercicio_4_4/src/Main.kt
+++ Ejercicio_4_4/src/Main.kt
@@ -1,5 +1,5 @@
 /*
-Ejercicio 4.4
+EJERCICIO 4.4
 
 Crear una clase Coche, a través de la cual se pueda conocer el color del coche, la marca, el modelo, el número de caballos, el número de puertas y la matrícula. Crear el constructor del coche, así como el método toString().
 
@@ -20,52 +20,44 @@ Intenta instanciar y modificar con el número de caballos con un valor inferior 
 Intenta instanciar y modificar con el número de puertas con un valor inferior a 3 o superior a 5 y comprueba que no es posible.
 Intenta instanciar y modificar con la matrícula con un valor que no tenga 7 caracteres y comprueba que no es posible.
 Intenta instanciar y modificar con el color, el número de caballos, el número de puertas y la matrícula con valores nulos/blancos y comprueba que no es posible.
-
  */
 
 fun main() {
-    // Instancia de varios coches
     val coche1 = Coche("Toyota", "Corolla", "Rojo", 150, 4, "1234567")
     val coche2 = Coche("Honda", "Civic", "Azul", 100, 5, "7654321")
 
-    // Mostrar coches por pantalla
+    // Muestra coches por pantalla
     println(coche1)
     println(coche2)
 
-    // Intentar modificar valores con condiciones incorrectas
-    // Modificar número de caballos a un valor inferior a 70
+    // Intenta modificar los valores con condiciones incorrectas
     try {
         coche1.color = ("Verde")
-        coche1.numCaballos = (50) // Esto debería lanzar una excepción
+        coche1.numCaballos = (50)
     } catch (e: IllegalArgumentException) {
         println("Error: ${e.message}")
     }
 
-    // Modificar número de puertas a un valor superior a 5
     try {
-        coche1.numPuertas = (6) // Esto debería lanzar una excepción
+        coche1.numPuertas = (6)
     } catch (e: IllegalArgumentException) {
         println("Error: ${e.message}")
     }
 
-    // Modificar matrícula con un valor que no tiene 7 caracteres
     try {
-        coche1.matricula = ("AB12345") // Esto debería lanzar una excepción
+        coche1.matricula = ("AB12345")
     } catch (e: IllegalArgumentException) {
         println("Error: ${e.message}")
     }
 
-    // Intentar instanciar o modificar con valores nulos/blancos
     try {
-        // Intentar instanciar con marca nula
-        val coche3 = Coche("", "Focus", "Blanco", 120, 4, "ABC1234") // Esto debería lanzar una excepción
+        val coche3 = Coche("", "Focus", "Blanco", 120, 4, "ABC1234")
     } catch (e: IllegalArgumentException) {
         println("Error: ${e.message}")
     }
 
-    // Intentar modificar con color nulo (ahora se permite porque color es String?)
     try {
-        coche1.color = ("") // Esto debería ejecutarse sin problemas
+        coche1.color = ("")
     } catch (e: IllegalArgumentException) {
         println("Error: ${e.message}")
     }
